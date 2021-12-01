881d8b77d4fe07651c2088d0a0580aba05693408
IpsyJayt <ipsyjayt@gmail.com>
2021-12-01 11:23:48 +0300

    add toggle slide to contact info

---
+++ contact/js/contact.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("h3#delTeam").click(function(){
+        $("#delInfo").slideToggle()
+    });
+    $("h3#techDesignTeam").click(function(){
+        $("#techDesignInfo").slideToggle()
+    });
+    $("h3#qualityTeam").click(function(){
+        $("#qualityInfo").slideToggle()
+    });
+    $("h3#careTeam").click(function(){
+        $("#careDeskInfo").slideToggle()
+    });
+
+});
